aad0cc3ba109060aba20dc9aa6df8bf9cfefd56a
Seriously Blank <siriusblackrc@gmail.com>
2025-10-13 14:12:14 +0530

    l4 done

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
+
+if len(sys.argv) > 2:
+	print("prompt not in quotes :)")
+	sys.exit(2)
+
+if len(sys.argv) == 1:
+	print("no prompt provided")
+	sys.exit(1)
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,7 +18,7 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-	model ='gemini-2.0-flash-001', contents ='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+	model ='gemini-2.0-flash-001', contents = sys.argv[1]
 )
 
 print(response.text)
